34f165d00d13bc3d38b46acdc3b7164fd86d075a
Mitch Gaines <gainesmitch2@gmail.com>
2019-04-29 20:33:38 -0400

    spelling fixes

--- client/src/SearchBar/SearchBar.js
+++ client/src/SearchBar/SearchBar.js
@@ -4,8 +4,8 @@ const searchbar = (props) => {
 
     return(
         <div className="form-group">
-            <input type="text" onChange={props.changed} className="search form-control col-sm-8 offset-2 d-none d-sm-block" placeholder="Buscar una especies" />
-            <input type="text" onChange={props.changed} className="search form-control col-xs-12 d-block d-sm-none" placeholder="Buscar una especies" />
+            <input type="text" onChange={props.changed} className="search form-control col-sm-8 offset-2 d-none d-sm-block" placeholder="Buscar una especie" />
+            <input type="text" onChange={props.changed} className="search form-control col-xs-12 d-block d-sm-none" placeholder="Buscar una especie" />
         </div>
     )
 }
